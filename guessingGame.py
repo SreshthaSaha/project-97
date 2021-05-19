2fb99d07dc1d536cd4cb39cd927be73dc412c8fe
SreshthaSaha <65612941+SreshthaSaha@users.noreply.github.com>
2021-05-19 16:32:34 +0530

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -2,18 +2,22 @@ import random
 number = random.randint(1, 9)
 print("Number Guessing Game")
 print("Guess a number between 1-9")
-guess = input("Enter the number: ")
-chances = 5
+guess = int(input("Enter the number: "))
+chances = 0
 
-while chances > 0:
-    if (guess  == number & chances > 0):
+while chances < 5:
+    if guess  == number :
         print("Congratulations YOU WON!!!!!!!")    
         break    
-    if(guess != number):
-        print("Try Again") 
-        break
-    elif chances == 0:
-        print("You Lose!! The number is " , number)    
+    elif guess > number:
+        print("Try Again.The number is too big")         
+    else:
+        print("Try again.The number is too small")
+
+    chances = chances + 1
+
+if chances == 5:
+    print("You Lose!! The number is " , number)     
         
     
     
